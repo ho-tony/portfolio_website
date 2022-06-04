460788b27ade38a33493637636450b20f335da06
ho-tony <ho.tony787@gmail.com>
2022-06-04 09:37:34 -0400

    added color border and about header button

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,15 +10,17 @@ function Home() {
     <div class="flex h-screen flex-col bg-[#333745]">
       <Header />
       <div class="grid place-content-center flex-grow place-items-center">
-     
-        <p class="text-center text-4xl text-[#FFFDF7] font-sans-serif tracking-[.5em] select-none font-bold my-10">
-          Tony Ho
+        <p class="text-center text-3xl text-[#FFFDF7] font-serif tracking-[.5em] select-none font-bold my-10">
+          TONY HO
         </p>
+        <div class="p-1 bg-gradient-to-tr from-amber-500 to-fuchsia-700 rounded-full">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4D03AQHJznxACTfnOw/profile-displayphoto-shrink_200_200/0/1647018597257?e=1659571200&v=beta&t=ZbbSnNadfLbGVvHewVRspkoTakFKeF0bOqzDyLIYqPI"
+            class="rounded-full"
+          />
+        </div>
 
-        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHJznxACTfnOw/profile-displayphoto-shrink_200_200/0/1647018597257?e=1659571200&v=beta&t=ZbbSnNadfLbGVvHewVRspkoTakFKeF0bOqzDyLIYqPI" class="rounded-full" alt=""/>
-
-
-        <p class="my-8 text-center text-2xl text-[#FFFDF7] font-sans-serif font-bold tracking-[0.25em] select-none">
+        <p class="my-8 text-center text-3xl text-[#FFFDF7] font-serif font-bold tracking-[0.25em] select-none">
           Full Stack Software Engineer
         </p>
         <IconBar />
